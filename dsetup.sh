0d9cee259593e6535153d48f85926143e8ffeee2
RockinWool <tamkaz23@yahoo.co.jp>
2020-04-15 20:00:31 +0900

    first commit

---
+++ dsetup.sh
@@ -0,0 +1,24 @@
+export PWD=$(pwd)
+export EXISTDOCKER=$(sudo docker ps -a -f "name=HugoC" --format {{.Names}})
+for X in "RockinWool"
+do
+    sudo docker build -t hugoc .
+    if [ "$EXISTDOCKER" = HugoC ]; then
+        read -p " すでにコンテナは存在しますが作り直しますか? (y/N):" BUILD
+        case "$BUILD" in 
+            [yY]*) sudo docker rm HugoC && sudo docker run -itd --name HugoC -v $PWD/volume/:/volume hugoc && sudo docker exec -it HugoC bash;;
+            *) echo "skipped." ;;
+        esac
+    else
+        sudo docker run -itd --name HugoC -v $PWD/volume/:/volume hugoc
+    fi
+    sudo docker start HugoC
+    sudo docker exec -it HugoC bash
+    sudo docker stop HugoC
+    read -p "${X} : コンテナを破棄しますか? (y/N):" VAR
+    case "$VAR" in 
+        [yY]*) sudo docker rm HugoC ;;
+        *) echo "skipped." ; continue ;;
+    esac
+    
+done
